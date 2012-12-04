0300c6d8704f37ad69467f20ec4679cff9cdca1d
Marko Elezović <marko@element.hr>
2012-12-04 15:49:09 +0100

    oaeu

--- php/web/app.php
+++ php/web/app.php
@@ -4,9 +4,9 @@ require_once __DIR__.'/../platform/Bootstrap.php';
 use Workshop\Person;
 
 $marko = new Person(array(
-	'firstName' => 'Tester',
-	'lastName' => 'Bester',
-	'birthdate' => '1987-6-5'
+    'firstName' => 'Tester',
+    'lastName' => 'Bester',
+    'birthdate' => '1987-6-5'
 ));
 
 $marko->persist();
@@ -14,7 +14,7 @@ $marko->persist();
 $everyone = Person::findAll();
 
 foreach($everyone as $person) {
-	echo 'My name is ', $person->firstName, ' and was born on ', $person->birthdate, '<br />';
+    echo 'My name is ', $person->firstName, ' and was born on ', $person->birthdate, '<br />';
 }
 ?>
 
