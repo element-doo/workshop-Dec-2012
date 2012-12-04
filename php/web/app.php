d635a45b0a19c9da122219b0a17d56e11c0e0b11
Marko Elezović <marko@element.hr>
2012-12-04 15:41:01 +0100

    helpers

--- php/web/app.php
+++ php/web/app.php
@@ -1,5 +1,6 @@
 <?php
 require_once __DIR__.'/../platform/Bootstrap.php';
+require_once __DIR__.'/helpers.php';
 
 use Workshop\Person;
 
@@ -9,48 +10,17 @@ $marko = new Person(array(
 	'birthdate' => '1987-6-5'
 ));
 
-$marko->persist();
+// $marko->persist();
+
+for($i = 0; $i < 10; $i++) {
+  $p = generateRandomPerson();
+  $p->persist();
+}
 
 $everyone = Person::findAll();
 
 foreach($everyone as $person) {
 	echo 'My name is ', $person->name, ' and was born on ', $person->birthdate, '<br />';
 }
-
-$shortNamed = Person::getShortPeople();
-
-echo '<h1>Short ppl</h1>';
-foreach($shortNamed as $person) {
-	echo 'My name is ', $person->name, ' and I\'m a short-named person!<br />';
-}
-
-// ------------------------------------------
-
-function randomWord() {
-	$randomWord = '';
-	$n = rand(3,7);
-	for($i = 0; $i < $n; $i++)
-		$randomWord .= chr(rand(ord('a'), ord('z')));
-	$randomWord = ucfirst($randomWord);
-
-	return $randomWord;
-}
-
-function generateRandomPerson() {
-	$randomDate = new DateTime();
-	$start = new DateTime('1970-1-1');
-	$end = new DateTime('2000-12-31');
-	$randomSecond = rand($start->getTimestamp(), $end->getTimestamp());
-	$randomDate = $randomDate->setTimestamp($randomSecond);
-
-	$p = new Person();
-  $p->firstName = randomWord();
-  $p->lastName = randomWord();
-  $p->birthdate = $randomDate;
-  return $p;
-}
-
-// ------------------------------------------
-
 ?>
 
---
+++ php/web/helpers.php
@@ -0,0 +1,26 @@
+<?php
+use Workshop\Person;
+
+function randomWord() {
+	$randomWord = '';
+	$n = rand(3,7);
+	for($i = 0; $i < $n; $i++)
+		$randomWord .= chr(rand(ord('a'), ord('z')));
+	$randomWord = ucfirst($randomWord);
+
+	return $randomWord;
+}
+
+function generateRandomPerson() {
+	$randomDate = new DateTime();
+	$start = new DateTime('1970-1-1');
+	$end = new DateTime('2000-12-31');
+	$randomSecond = rand($start->getTimestamp(), $end->getTimestamp());
+	$randomDate = $randomDate->setTimestamp($randomSecond);
+
+	$p = new Person();
+  $p->firstName = randomWord();
+  $p->lastName = randomWord();
+  $p->birthdate = $randomDate;
+  return $p;
+}
