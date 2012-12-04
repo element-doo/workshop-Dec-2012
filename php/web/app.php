45c4c30a907f2d3f5670650502669809a8597239
Marko Elezović <marko@element.hr>
2012-12-04 15:47:02 +0100

    oaeu

--- php/web/app.php
+++ php/web/app.php
@@ -4,23 +4,37 @@ require_once __DIR__.'/helpers.php';
 
 use Workshop\Person;
 
-$marko = new Person(array(
-	'firstName' => 'Tester',
-	'lastName' => 'Bester',
-	'birthdate' => '1987-6-5'
+$tester = new Person(array(
+    'firstName' => 'Tester',
+    'lastName' => 'Bester',
+    'birthdate' => '1987-6-5'
 ));
 
-// $marko->persist();
+// $tester->persist();
 
-for($i = 0; $i < 10; $i++) {
-  $p = generateRandomPerson();
-  $p->persist();
+$randomPersons = array();
+for($i = 0; $i < 50; $i++) {
+    $randomPersons[] = generateRandomPerson();
 }
+
+use NGS\Client\StandardProxy;
+$proxy = new StandardProxy();
+// $proxy->insert($randomPersons);
 
 $everyone = Person::findAll();
 
 foreach($everyone as $person) {
-	echo 'My name is ', $person->name, ' and was born on ', $person->birthdate, '<br />';
+    echo 'My name is ', $person->name, ' and was born on ', $person->birthdate, '<br />';
 }
+
+/*
+$proxy->delete($everyone);
+
+// Query again - all people should be gone
+$everyone = Person::findAll();
+*/
+
+echo 'There are '.count($everyone).' people available.';
+
 ?>
 
