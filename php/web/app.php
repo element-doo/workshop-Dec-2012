b2f7f058536ff785582a771a1dc34fc6390c56dd
Marko Elezović <marko@element.hr>
2012-12-04 15:48:23 +0100

    aoeu

--- php/web/app.php
+++ php/web/app.php
@@ -5,22 +5,22 @@ require_once __DIR__.'/helpers.php';
 use Workshop\Person;
 
 $marko = new Person(array(
-	'firstName' => 'Tester',
-	'lastName' => 'Bester',
-	'birthdate' => '1987-6-5'
+    'firstName' => 'Tester',
+    'lastName' => 'Bester',
+    'birthdate' => '1987-6-5'
 ));
 
 // $marko->persist();
 
 for($i = 0; $i < 10; $i++) {
-  $p = generateRandomPerson();
-  $p->persist();
+    $p = generateRandomPerson();
+    $p->persist();
 }
 
 $everyone = Person::findAll();
 
 foreach($everyone as $person) {
-	echo 'My name is ', $person->name, ' and was born on ', $person->birthdate, '<br />';
+    echo 'My name is ', $person->name, ' and was born on ', $person->birthdate, '<br />';
 }
 ?>
 
