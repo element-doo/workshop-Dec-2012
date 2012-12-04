b5d21562330051d641b3abc10f8e00f9e80179b9
Marko Elezović <marko@element.hr>
2012-12-04 15:37:48 +0100

    celanup

--- php/web/app.php
+++ php/web/app.php
@@ -16,32 +16,5 @@ $everyone = Person::findAll();
 foreach($everyone as $person) {
 	echo 'My name is ', $person->firstName, ' and was born on ', $person->birthdate, '<br />';
 }
-
-// ------------------------------------------
-
-function randomWord() {
-	$randomWord = '';
-	$n = rand(3,7);
-	for($i = 0; $i < $n; $i++)
-		$randomWord .= chr(rand(ord('a'), ord('z')));
-	$randomWord = ucfirst($randomWord);
-
-	return $randomWord;
-}
-
-function generateRandomPerson() {
-	$randomDate = new DateTime();
-	$start = new DateTime('1970-1-1');
-	$end = new DateTime('2000-12-31');
-	$randomSecond = rand($start->getTimestamp(), $end->getTimestamp());
-	$randomDate = $randomDate->setTimestamp($randomSecond);
-
-	$p = new Person();
-  $p->firstName = randomWord();
-  $p->lastName = randomWord();
-  $p->birthdate = $randomDate;
-  return $p;
-}
-
 ?>
 
