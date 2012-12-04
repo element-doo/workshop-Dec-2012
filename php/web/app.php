2dc7bd352b8e2b2333138f115088543971d44fc4
Marko Elezović <marko@element.hr>
2012-12-04 15:49:54 +0100

    oaeu

--- php/web/app.php
+++ php/web/app.php
@@ -3,13 +3,13 @@ require_once __DIR__.'/../platform/Bootstrap.php';
 
 use Workshop\Person;
 
-$marko = new Person(array(
+$tester = new Person(array(
     'firstName' => 'Tester',
     'lastName' => 'Bester',
     'birthdate' => '1987-6-5'
 ));
 
-$marko->persist();
+$tester->persist();
 
 $everyone = Person::findAll();
 
